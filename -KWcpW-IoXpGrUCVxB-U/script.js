f88adcc2def58f01e0a73b6aff68199032b12477
katya20 <kpanchenko21@gmail.com>
2016-11-16 08:32:34 -0800

    Classadoo autocommit

--- -KWcpW-IoXpGrUCVxB-U/script.js
+++ -KWcpW-IoXpGrUCVxB-U/script.js
@@ -39,7 +39,7 @@ function addDiamond() {
 function addDiamondRandomly() {
   var position = Math.floor((Math.random() * 400) + 1);
   var size = Math.floor((Math.random() * 400) + 1);
-  var color = Math.floor((Math.random() * 400) + 1);
+//  var color = Math.floor((Math.random() * 3) + 0);
   var time = 1500
   diamond.css("top", position)
   diamond.css('left', position)
